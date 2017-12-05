039d9c88a347a155863d48850e45fbb449f7d62e
Eric Ellingson <ellingsonster@gmail.com>
2017-12-04 22:56:57 -0800

    ship lights

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
+const express = require('express');
 const app = express();
 
-const config = require("./config");
-
+const config = require('./config');
 
 const routes = {
-	index: require("./routes/index"),
+  index: require('./routes/index')
 };
 
-app.use("/static", express.static(path.resolve("./static")));
-app.use("/", routes.index);
+app.use('/static', express.static(path.resolve('./static')));
+app.use('/', routes.index);
 
-
-app.listen(config.port, () => console.log(`listening on http://localhost:${config.port}/`));
+app.listen(config.port, () =>
+  console.log(`listening on http://localhost:${config.port}/`)
+);
--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,31 @@
 (function() {
-	window.onload = main;
-	function createScaleSelect() {
-		let scales = Notes.scales;
-		let select = document.querySelector("#scales");
-		Object.keys(scales).forEach(scale => {
-			const option = document.createElement("option");
-			option.value = scale;
-			option.innerText = scale;
-			select.appendChild(option);
-		});
-		select.addEventListener("change", function(e) {
-			Game.Sound.scale = Notes.selectScaleByName(this.value);
-		});
-	}
-	function main() {
-		createScaleSelect();
-	}
+  window.onload = main;
+  function createScaleSelect() {
+    let scales = Notes.scales;
+    let select = document.querySelector('#scales');
+    Object.keys(scales).forEach(scale => {
+      const option = document.createElement('option');
+      option.value = scale;
+      option.innerText = scale;
+      select.appendChild(option);
+    });
+    select.addEventListener('change', function(e) {
+      Game.Sound.scale = Notes.selectScaleByName(this.value);
+    });
+  }
+  function main() {
+    let cvs = document.querySelector('#canvas');
+    let ships = [
+      'url(/static/images/spaceship0.png) 50 23, auto',
+      'url(/static/images/spaceship1.png) 50 23, auto',
+      'url(/static/images/spaceship2.png) 50 23, auto'
+    ];
+    let shipIndex = 0;
+    setInterval(function() {
+      let ship = ships.shift();
+      cvs.style.cursor = ship;
+      ships.push(ship);
+    }, 1000);
+    createScaleSelect();
+  }
 })();
