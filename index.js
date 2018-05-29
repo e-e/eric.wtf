65f970fcdc5e125269a1356c89fa4df2f41682aa
Eric Ellingson <ellingsonster@gmail.com>
2018-05-28 20:37:48 -0700

    added slackmojify route

--- index.js
+++ index.js
@@ -7,16 +7,20 @@ const utils = require('./node_modules/ghost/core/server/utils');
 const config = require('./config');
 
 const routes = {
-  index: require('./routes/index')
+  index: require('./routes/index'),
+  slackmojify: require('./routes/slackmojify')
 };
 
 ghost().then(function(ghostServer) {
   app.use('/ramen', ghostServer.rootApp);
   ghostServer.start(app);
   app.use('/static', express.static(path.resolve('./static')));
+  app.use('/slackmojify', routes.slackmojify);
   app.use('/', routes.index);
 });
 
 app.listen(config.port, () =>
   console.log(`listening on http://localhost:${config.port}/`)
 );
+
+module.exports = app;
